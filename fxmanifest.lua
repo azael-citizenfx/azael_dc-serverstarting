44b3263f52b9e4cdde7cc88c06c256f8ff220a71
Azael Dev <42119584+Azael-Dev@users.noreply.github.com>
2025-05-10 13:33:39 +0700

    chore(fxmanifest): add RedM (RDR3) support.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
-game 'gta5'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Azael Dev <contact@azael.dev> (https://www.azael.dev/)'
 description 'DC - Server Starting (Announcement)'
-version '1.1.4'
+version '1.1.5'
 url 'https://github.com/Azael-Dev/azael_dc-serverstarting'
 
 lua54 'yes'
